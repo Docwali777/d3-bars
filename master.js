4a262b4ff7e2272a464709d69e228378c5d88480
docwali777 <drgauvin@mgmcare.org>
2017-01-02 23:22:12 -0500

    Interactive Data

--- master.js
+++ master.js
@@ -1,7 +1,7 @@
 var url  = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
 
 var h = 400;
-var w = 500;
+var w = 900;
 
 d3.json(url, function(data){
   var datum = data.data
@@ -13,9 +13,10 @@ var yScale = d3.scaleLinear()
               .domain([0, heightMax])
               .range([0, h])
 
-var xScale = d3.scaleLinear()
-              .domain([0, datum.length])
+var xScale = d3.scaleBand()
+              .domain(d3.range(datum.length))
               .range([0, w])
+               .paddingInner(0.05);
 
 
 
@@ -23,13 +24,45 @@ var svg = d3.select("body").append("svg").attr("height", h).attr("width", w)
 
 var rect = svg.selectAll("rect").data(datum).enter().append("rect")
 
+
+
+
 rect.attr("height", function(d){return d[1]})
   .attr("y", function(d){return h-yScale(d[1])})
-    .attr("x", function(d, i){return xScale(i)})
-    .attr("width", 2)
+    .attr("x", function(d, i){return xScale(i)+20})
+    // .attr("width", function(d, i){return i})
+    .attr("width", xScale.bandwidth())
+    .attr("fill", "teal")
+          .on("mouseover", function() { d3.select(this).attr("fill", "black")
+})
+.on("mouseout", function(d) { d3.select(this)
+          .attr("fill", "teal")
+          .transition()
+          .duration(250)
 
-var tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d; });
-console.log(tip)
+    })
+
+rect.on("mouseover", function(d) {
+  var xPosition = parseFloat(d3.select(this).attr("x")) + xScale.bandwidth()/ 2;
+  var yPosition = parseFloat(d3.select(this).attr("y")) + 14;
+
+  svg.append("text")
+      .attr("id", "tooltip")
+      .attr("x", xPosition)
+      .attr("y", yPosition)
+      .attr("text-anchor", "middle")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "30px")
+      .attr("font-weight", "bold")
+      .attr("fill", "black")
+      .style("background-color", "white")
+.text(d); })
+.on("mouseout", function() { //Remove the tooltip
+    d3.select("#tooltip").remove();
+})
+
+
+
 
 
 
